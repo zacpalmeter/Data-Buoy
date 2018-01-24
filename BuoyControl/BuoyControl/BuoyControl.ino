1ae45c9b02ed7cb5888215deca904549172adffe
zacpalmeter <zacpalmeter@gmail.com>
2018-01-24 11:01:37 -0500

    Project File
    The project file for the arduino.  If you have Visual Micro open the .sln file but if you ant to use just the Arduino IDE just open the .ino file

---
+++ BuoyControl/BuoyControl/BuoyControl.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BuoyControl.ino
+ Created:	1/24/2018 10:59:20 AM
+ Author:	zacpa
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
