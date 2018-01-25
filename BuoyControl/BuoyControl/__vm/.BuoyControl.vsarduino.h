51146eeaf72466103810b91418aca8a17710bd86
joshagirgis <joshua.girgis@maine.edu>
2018-01-25 13:30:06 -0500

    Thermistor/ Kalman Filter
    Added thermistor and Kalman Filtering capabilities

--- BuoyControl/BuoyControl/__vm/.BuoyControl.vsarduino.h
+++ BuoyControl/BuoyControl/__vm/.BuoyControl.vsarduino.h
@@ -5,21 +5,18 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Yún, Platform=avr, Package=arduino
+	Hardware: Arduino/Genuino Mega w/ ATmega2560 (Mega 2560), Platform=avr, Package=arduino
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega32u4__
-#define __AVR_ATmega32U4__
+#define __AVR_ATmega2560__
 #define F_CPU 16000000L
-#define ARDUINO 106012
-#define ARDUINO_AVR_YUN
+#define ARDUINO 10805
+#define ARDUINO_AVR_MEGA2560
 #define ARDUINO_ARCH_AVR
-#define USB_VID 0x2341
-#define USB_PID 0x8041
 #define __cplusplus 201103L
 #define __AVR__
 #define __inline__
