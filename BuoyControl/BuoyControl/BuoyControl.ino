9e7ce57e1c1e8d09c833e2c42423772cf2a11bd8
joshagirgis <joshua.girgis@maine.edu>
2018-01-28 02:18:58 -0500

    Sleep()
    Added Sleep function, and cleaned things up

--- BuoyControl/BuoyControl/BuoyControl.ino
+++ BuoyControl/BuoyControl/BuoyControl.ino
@@ -5,40 +5,62 @@
  Author:  Zac Palmeter, Joshua Girgis, Austin Kidder, Kale Prentice, Justin Cottle
 */////////////////////////////////////////////////////////////////////////////////////
 #include <math.h> //Include math library
-double temperature[100];  // allocate memory for temperature reading
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#define dataLimit 100
+
+double temperature[dataLimit];  // allocate memory for temperature reading
 
 //Kalman Filtering Variables
 //Thermistor
-double T_p_k[100];   //init prior error covariance.
-double T_hat_k[100]; //init temperature estimation
+int tempPower = 13;
+double T_p_k[dataLimit];   //init prior error covariance.
+double T_hat_k[dataLimit]; //init temperature estimation
 double T_c;          //Measurement State space. Basically Thermistor is sole contributor
 double T_r;       //Measurement noise covariance. Thermistor fluctuates ? degrees on avg 
 double T_q;      //Process noise covariance for Thermistor
-int t_i;             //temp reading increment
+int d_i;             //Data reading increment
 /////////////////////////////////////////////////////////////////////////////////////
+
 
 void setup() {
   // the setup function runs once when you press reset or power the board
   Serial.begin(9600);
+  pinMode(tempPower, OUTPUT);          // sets the digital pin 13 as output
   initKalman();
 }
+
+// Watchdog Interrupt Service. This is executed when watchdog timed out.
+ISR(WDT_vect) 
+{
+  wdt_disable();  // disable watchdog
+}
+
+
+void enterSleep(const byte interval)
+{
+  MCUSR = 0;                          // reset various flags
+  WDTCSR |= 0b00011000;               // see docs, set WDCE, WDE
+  WDTCSR =  0b01000000 | interval;    // set WDIE, and appropriate delay
+
+  wdt_reset();
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
+  sleep_mode();            // now goes to Sleep and waits for the interrupt
+  } 
+
 
 void initKalman() {
   //Temperature Setup
    T_c = 1;    //Measurement State space. Basically Thermistor is sole contributor
-   T_r = 0.2;  //Measurement noise covariance. Thermistor fluctuates ? degrees on avg 
+   T_r = 0.55;  //Measurement noise covariance. Thermistor fluctuates .55 degrees on avg 
    T_q = 0.25; //Process noise covariance for Thermistor
-   t_i = 1;    //Start at 1 for first temp value for Kalman estimate
    T_p_k[0] = 1; // Thermistor's prior error covariance. Assume safe start at 1
-
-  //first value Temperature for Kalman Filter
-   temperature[0] =readThermistor(A0);
-   T_hat_k[0] = temperature[0];  // start at last known reading!
+   d_i = 1;    //Start at 1 for first increment value for Kalman estimate
 }
 
 
 double KalmanFilter(double c, double r, double q, double xhat_0, double z_k, double p_k_0, double p_k, double xhat_k) {
-// Stand alone Filters out a signal based on state space, noise estimates, and measurements
+// Stand alone Extended Kalman Filter, filters out a signal based on state space, noise estimates, and measurements
 // Provides one estimation point in real time
     double g; //init kalman gain
   //predict
@@ -58,27 +80,58 @@ double KalmanFilter(double c, double r, double q, double xhat_0, double z_k, dou
 double readThermistor(int sensorPin) {
   double temp;
   double sensorValue = 0;  // variable to store the value coming from the sensor
+  digitalWrite(tempPower,HIGH);
   //Reads Thermistor and Filters signal
   sensorValue = analogRead(sensorPin);  //Read Analog signal
   sensorValue = sensorValue * (5.0 / (1023));  //Convert to voltage
   //Calibration Equation
   temp = -12.531*pow(sensorValue, 3) + 118.81*pow(sensorValue, 2) - 413.27*sensorValue + 671.85;
+  
   return temp;
+}
+
+void on(){
+  // data taking on
+   //first value Temperature for Kalman Filter
+   temperature[0] =readThermistor(A0);
+   T_hat_k[0] = temperature[0];  // start at last known reading!
+
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  temperature[t_i] = readThermistor(A0);
-  T_hat_k[t_i] = KalmanFilter(T_c, T_r, T_q, T_hat_k[t_i - 1], temperature[t_i], T_p_k[t_i - 1],T_p_k[t_i], T_hat_k[t_i]);
- 
- //TESTING////////////////
+  // this function loops repeatedly until a data limit is reached then it sleeps in 
+  // a low power state and overwrites the data.
+  
+  on();  //inserted for seemless data loop
+  if (d_i>=dataLimit){           // when does data taking needs to stop?
+    //////////////////////////////////////////////////
+    //Insert time for an one hour later!
+    ///////////////////////////////////////////////////
+    enterSleep(0b100001);  // 8 seconds
+    enterSleep(0b100001);  // 8 seconds
+    enterSleep(0b100000);  // 4 seconds
+    ///////////////////////////////////////////////////
+    d_i=1; // turns on data taking again
+    }
+  else {
+    ///////////////////////////////////////////////////
+    //Insert Data Taking Below!
+    ///////////////////////////////////////////////////
+    temperature[d_i] = readThermistor(A0);    //take temperature data from A0 pin
+    //Filter temperature data using Extended Kalman Filter
+    T_hat_k[d_i] = KalmanFilter(T_c, T_r, T_q, T_hat_k[d_i - 1], temperature[d_i], T_p_k[d_i - 1],T_p_k[d_i], T_hat_k[d_i]);
+    ///////////////////////////////////////////////////
+    //TESTING
+    ///////////////////////////////////////////////////
   /**/
-  Serial.print(temperature[t_i]);
-  Serial.print("\t");
-  Serial.println(T_hat_k[t_i]);//*/
-//////////////////////////
-  t_i++;
-  if (t_i>=100) t_i=1;
-  delay(100);
+    //print data to plotter for testing
+    Serial.print(temperature[d_i]);
+    Serial.print("\t");
+    Serial.println(T_hat_k[d_i]);//*/
+    ///////////////////////////////////////////////////
+     d_i++;  //next data point
+    
+  }
+  delay(100);  // somewhat of a delay
 }
