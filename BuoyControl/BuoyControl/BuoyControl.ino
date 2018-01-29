1cb25d102bbfb20cef54a33367d1f21ceac8fea6
joshagirgis <joshua.girgis@maine.edu>
2018-01-29 10:58:56 -0500

    Took out old turbidity code

--- BuoyControl/BuoyControl/BuoyControl.ino
+++ BuoyControl/BuoyControl/BuoyControl.ino
@@ -10,6 +10,9 @@
 #define dataLimit 100
 
 double temperature[dataLimit];  // allocate memory for temperature reading
+double turbidity[dataLimit];  // allocate memory for turbidity reading
+
+int d_i;             //Data reading increment
 
 //Kalman Filtering Variables
 //Thermistor
@@ -19,7 +22,8 @@ double T_hat_k[dataLimit]; //init temperature estimation
 double T_c;          //Measurement State space. Basically Thermistor is sole contributor
 double T_r;       //Measurement noise covariance. Thermistor fluctuates ? degrees on avg 
 double T_q;      //Process noise covariance for Thermistor
-int d_i;             //Data reading increment
+//Turbidity Sensor
+
 /////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -50,12 +54,14 @@ void enterSleep(const byte interval)
 
 
 void initKalman() {
-  //Temperature Setup
+   d_i = 1;    //Start at 1 for first increment value for Kalman estimate
+   //Thermistor Setup
    T_c = 1;    //Measurement State space. Basically Thermistor is sole contributor
    T_r = 0.55;  //Measurement noise covariance. Thermistor fluctuates .55 degrees on avg 
    T_q = 0.25; //Process noise covariance for Thermistor
    T_p_k[0] = 1; // Thermistor's prior error covariance. Assume safe start at 1
-   d_i = 1;    //Start at 1 for first increment value for Kalman estimate
+   //Turbidity Sensor Setup
+  
 }
 
 
@@ -83,12 +89,13 @@ double readThermistor(int sensorPin) {
   digitalWrite(tempPower,HIGH);
   //Reads Thermistor and Filters signal
   sensorValue = analogRead(sensorPin);  //Read Analog signal
-  sensorValue = sensorValue * (5.0 / (1023));  //Convert to voltage
+  sensorValue = sensorValue * (5.0 / (1023.0));  //Convert to voltage
   //Calibration Equation
   temp = -12.531*pow(sensorValue, 3) + 118.81*pow(sensorValue, 2) - 413.27*sensorValue + 671.85;
   
   return temp;
 }
+
 
 void on(){
   // data taking on
@@ -106,11 +113,11 @@ void loop() {
   on();  //inserted for seemless data loop
   if (d_i>=dataLimit){           // when does data taking needs to stop?
     //////////////////////////////////////////////////
-    //Insert time for an one hour later!
+    //Insert time for one hour later!
     ///////////////////////////////////////////////////
-    enterSleep(0b100001);  // 8 seconds
-    enterSleep(0b100001);  // 8 seconds
-    enterSleep(0b100000);  // 4 seconds
+  //  enterSleep(0b100001);  // 8 seconds
+  //  enterSleep(0b100001);  // 8 seconds
+  //  enterSleep(0b100000);  // 4 seconds
     ///////////////////////////////////////////////////
     d_i=1; // turns on data taking again
     }
@@ -121,6 +128,7 @@ void loop() {
     temperature[d_i] = readThermistor(A0);    //take temperature data from A0 pin
     //Filter temperature data using Extended Kalman Filter
     T_hat_k[d_i] = KalmanFilter(T_c, T_r, T_q, T_hat_k[d_i - 1], temperature[d_i], T_p_k[d_i - 1],T_p_k[d_i], T_hat_k[d_i]);
+   
     ///////////////////////////////////////////////////
     //TESTING
     ///////////////////////////////////////////////////
